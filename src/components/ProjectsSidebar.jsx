135bd110fe54e9ce899e9eeb98556507deb05b5a
Robert Gorman <rbtgorman@gmail.com>
2024-07-31 16:07:46 -0400

    Updating Sidebar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,10 +1,14 @@
+import Button from './Button.jsx';
+
+
 export default function ProjectsSidebar() {
     return (
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone md:w-72 rounded-r-xl">
         <h2 className ="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
         <div>
-            <button button className="px-4 py-2 text-stone-200 hover:text-stone-100 hover:bg-stone-800 rounded-md transition-colors"
-            > + Add Project</button>
+            <Button>
+                + Add Project
+            </Button>
         </div>
         <ul>
 
@@ -13,3 +17,5 @@ export default function ProjectsSidebar() {
     );
 
 }
+
+/*text-stone-200 hover:text-stone-100 hover:bg-stone-800 rounded-md transition-colors*/
