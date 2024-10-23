ac2b92b0709cc51ae1149bb4db8e1bc2097eb319
Robert Gorman <rbtgorman@gmail.com>
2024-10-23 14:28:29 -0400

    add anzgroupnj.com project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 function Projects() {
   const projects = [
+
+    {
+      title: "Web Application for General Contracting Business",
+      description: 
+      "Developed a web app for a client's general contracting business consumers to change utility service to a new location using Flask, SQL, Python, and HTML"
+      + "The website is a starter kit designed for creating a blog using Eleventy and Netlify CMS, providing a user-friendly interface for clients to manage content." 
+      + "It includes pre-configured scripts and settings, allowing users to quickly set up a responsive blog site on Netlify by enabling features like Identity and Git Gateway" 
+      + "for seamless content submission to the repository. The kit leverages Eleventy's templating capabilities, including reusable components, centralized data management, "
+      + "and image optimization plugins, ensuring efficient development. Users can easily modify the project structure, add new pages, and optimize images while benefiting"
+      + "from built-in features like dark mode and navigation management. Overall, this kit streamlines the process of building and maintaining a blog with minimal setup time.",
+       link: "https://enzgroupnj.com/"      
+    },
+
     {
       title: "Water America Web App",
       description: 
