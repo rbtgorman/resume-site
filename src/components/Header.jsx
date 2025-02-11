50b1aba5b6005b28f44517526dbad83273efbc56
Robert Gorman <54149803+rbtgorman@users.noreply.github.com>
2025-02-11 13:26:50 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="max-w-4xl mx-auto mt-16 p-8 bg-gradient-to-br from-green-100 to-blue-100 shadow-2xl rounded-lg overflow-hidden relative">
       <div className="absolute top-0 left-0 w-full h-2 bg-gradient-to-r from-orange-400 to-pink-400"></div>
       <h1 className="text-4xl font-bold text-center text-blue-800 mb-2">Robert Gorman</h1>
-      <p className="text-xl text-center text-blue-600">AWS Cloud PractitionerCertified Web Developer/Data Engineer</p>
+      <p className="text-xl text-center text-blue-600">Certified AWS Cloud Practitioner - Web Developer/Data Engineer</p>
     </header>
   );
 }
