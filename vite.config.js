43cc348e514640b4f0e956d9877bf367d93a0873
Robert Gorman <rbtgorman@gmail.com>
2025-02-19 13:20:24 -0500

    change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/resume-site/',
-  build: {
-    outDir: 'dist',
-  },
-})
+  base: '/resume-site/', // This ensures assets are loaded correctly
+});
+
